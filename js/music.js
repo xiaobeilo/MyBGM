049f11449825557159eda5dcfcb705e3224e29bc
cMing <mayday404@163.com>
2016-09-21 18:06:26 +0800

    添加了官方对象 未修改完成

--- js/music.js
+++ js/music.js
@@ -1,4 +1,5 @@
 /// <reference path="jquery.d.ts" />
+var SIZE = 32;
 var Music = (function () {
     function Music(type, count) {
         this.currentSong = 0;
@@ -22,6 +23,12 @@ var Music = (function () {
         this.STOP = 0;
         this.audio = $('#audio')[0];
         var me = this;
+        this.visualizer = new MusicVisualizer({
+            size: SIZE,
+            onended: function () {
+                me.playTo('next');
+            }, visualizer: Render()
+        });
         this.theme.type = type;
         this.theme.count = count;
         this.initList();
@@ -140,8 +147,8 @@ var Music = (function () {
         clearInterval(this.spePre.timer);
         this.spe.attr('src', "images/special/" + this.theme.type + "/" + this.allSongData[this.currentSong].spe_en + ".jpg");
         var me = this;
+        this.visualizer.play(this.allSongData[idx].qiniu_src, false);
         // load(this.allSongData[idx].qiniu_src);
-        mv.play(this.allSongData[idx].qiniu_src);
         // this.audio.src = this.allSongData[idx].qiniu_src;
         var lyricSrc = "./data/" + this.theme.type + "/" + this.allSongData[idx].lrc_name + ".lrc";
         me.getLyric(lyricSrc);
@@ -275,33 +282,94 @@ var Music = (function () {
     };
     return Music;
 }());
-var size = 128;
-var mv = new MusicVisualizer({
-    size: size,
-    visualizer: draw
-});
-$('#volume').change(function () {
-    mv.changeVolume(this.value / this.max);
-});
-var ctx = $('canvas')[0].getContext('2d');
-function resize() {
-    $('canvas').width($('#mainc').width()).height($('#mainc').height() - 5);
-    var line = ctx.createLinearGradient(0, 0, 0, 295);
-    line.addColorStop(0, 'red');
-    line.addColorStop(0.5, 'yellow');
-    line.addColorStop(1, 'green');
-    ctx.fillStyle = line;
+var canvas = $('canvas')[0];
+//宽高暂时未设置
+var ctx = canvas.getContext('2d');
+var HEIGHT;
+var WIDTH;
+var cw;
+function init() {
+    HEIGHT = $('#mainc').height();
+    WIDTH = $('#mainc').width();
+    canvas.height = HEIGHT;
+    canvas.width = WIDTH;
 }
-$(window).resize(function () {
-    resize();
-});
-function draw(arr) {
-    ctx.clearRect(0, 0, 1920, 295);
-    var w = $('canvas').width() / size;
-    for (var i = 0; i < size; i++) {
-        var h = arr[i] / 256 * $('canvas').height();
-        ctx.fillRect(w * i, $('canvas').height() - h, w * 0.6, h);
+var ARR = [];
+ARR.dotMode = 'random';
+function getArr() {
+    ARR.length = 0;
+    ARR.linearGradient = ctx.createLinearGradient(0, 295, 0, 0);
+    ARR.linearGradient.addColorStop(0, 'green');
+    ARR.linearGradient.addColorStop(0.5, '#ff0');
+    ARR.linearGradient.addColorStop(1, '#f00');
+    for (var i = 0; i < SIZE; i++) {
+        var x = random(0, WIDTH), y = random(0, HEIGHT), color = "rgba(" + random(100, 250) + "," + random(50, 250) + "," + random(50, 100) + ",0)", ran = random(1, 8) * 0.2;
+        ARR.push({
+            x: x,
+            y: y,
+            color: color,
+            dx: ARR.dotMode == 'random' ? ran : 0,
+            dx2: ran,
+            dy: random(1, 5),
+            cap: 0,
+            cheight: 10
+        });
     }
+}
+$(window).resize(init);
+Render.type = 'Column';
+function random(min, max) {
+    min = min || 0;
+    max = max || 1;
+    return max >= min ? Math.round(Math.random() * (max - min) + min) : 0;
+}
+function Render() {
+    var o = null;
+    return function (del, ave) {
+        ctx.fillStyle = ARR.linearGradient;
+        var w = Math.round(WIDTH / SIZE), cgap = Math.round(w * 0.3);
+        cw = w - cgap;
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        if (Render.type == 'Dot' && ((del > 3 && ave > 30) || (ave > 50 && del > 0))) {
+            var d = Math.round(del * (ave - 20) * 0.01) + 4;
+            for (var i = 0; i < d; i++) {
+                var y = random(-HEIGHT * 2, 3 * HEIGHT);
+                ctx.beginPath();
+                ctx.moveTo(0, y);
+                ctx.lineTo(WIDTH, HEIGHT - y);
+                ctx.strokeStyle = "rgba(" + random(100, 250) + "," + random(50, 250) + "," + random(50, 100) + ")";
+                ctx.stroke();
+            }
+        }
+        for (var i = 0; i < SIZE; i++) {
+            o = ARR[i];
+            if (Render.type == 'Dot') {
+                var x = o.x, y = o.y, r = Math.round((this[i] / 2 + 18) * (HEIGHT > WIDTH ? WIDTH : HEIGHT) / 600); /*这里省略了移动端的判断*/
+                o.x += o.dx;
+                o.x > WIDTH && (o.x = 0);
+                //开始路径，绘画圆
+                ctx.beginPath();
+                ctx.arc(x, y, r, 0, Math.PI * 2, true);
+                var gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
+                gradient.addColorStop(0, "rgb(255,255,255)");
+                ctx.fillStyle = gradient;
+                ctx.fill();
+            }
+            if (Render.type == 'Column') {
+                var h = this[i] / 280 * HEIGHT;
+                ARR[i].cheight > cw && (ARR[i].cheight = cw);
+                if (--ARR[i].cap < ARR[i].cheight) {
+                    ARR[i].cap = ARR[i].cheight;
+                }
+                if (h > 0 && (ARR[i].cap < h + 40)) {
+                    ARR[i].cap = h + 40 > HEIGHT ? HEIGHT : h + 40;
+                }
+                //console.log(ARR[i].cap);
+                ctx.fillRect(w * i, HEIGHT - ARR[i].cap, cw, ARR[i].cheight);
+                ctx.fillRect(w * i, HEIGHT - h, cw, h);
+            }
+        }
+    };
 }
 var music = new Music('mayday', 20);
 $('#switch .dropdown-menu').delegate('a', 'click', function () {
